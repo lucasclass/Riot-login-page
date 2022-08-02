69e74a23ddb84948f8a1c76acde607e9f9543228
Lucas Class <lucasclass94@gmail.com>
2022-08-02 08:24:36 -0300

    ♻️ refactor : update  development scripts

--- script.js
+++ script.js
@@ -2,31 +2,27 @@ const inputs = document.querySelectorAll('.input')
 const button = document.querySelector('.login__button')
 
 const handleFocus = ({ target }) => {
-  const span = target.previousElementSibling
+  const span = target.previousElementSibling;
   span.classList.add('span-active')
 }
 
 const handleFocusOut = ({ target }) => {
-
-  if (target.value === ''){
-  const span = target.previousElementSibling
-  span.classList.remove('span-active')
+  if (target.value === '') {
+    const span = target.previousElementSibling;
+    span.classList.remove('span-active')
   }
 }
 
 const handleChange = () => {
   const [username, password] = inputs
 
-if (username.value && password.value >= 8){
-  button.removeAttribute('disabled')
-} else {
-  button.setAttribute('disabled', '')
-}
-
+  if (username.value && password.value.length >= 8) {
+    button.removeAttribute('disabled')
+  } else {
+    button.setAttribute('disabled', '')
+  }
 }
 
 inputs.forEach((input) => input.addEventListener('focus', handleFocus))
-inputs.forEach((input) => input.addEventListener('focus', handleFocusOut))
-
-inputs.forEach((input) => input.addEventListener('focus', handleChange))
-
+inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut))
+inputs.forEach((input) => input.addEventListener('input', handleChange))
